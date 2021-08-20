38b24f2e3760c4bf8d6290ec5652c80a06ed0bfd
Jayasri Palanisamy <jayasrip953@gmail.com>
2021-08-20 15:34:12 +0530

    Add Serial_comm_PyQt5.py
    The code is made clean by removing functions and creating classes

--- Serial_comm_PyQt5.py
+++ Serial_comm_PyQt5.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 import serial
-
+import sys
 
 serial_port_para = {'port': 'COM10', 'baudrate': '9600'}
 
@@ -17,68 +17,68 @@ def serial_ports():
             pass
     return result
 
-def set_serial_port():
-    serial_port_para['port'] = com_b.currentText()
+# Creating main window
+class Window(QWidget):
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.win = QWidget()
+        self.win.setGeometry(500, 200, 300, 250)
+        self.win.setWindowTitle("UART serial values")
+        super().__init__()
 
-def set_baud_rate():
-    serial_port_para['baudrate'] = com_b_1.currentText()
+        # creating form
+        self.formGroupBox = QGroupBox("Parameters:")
+        self.layout = QFormLayout()
+        self.com_b_1 = QComboBox()
+        self.com_b_1.addItems(serial_ports())
+        self.com_b_2 = QComboBox()
+        self.com_b_2.addItems(['9600', '1200', '2400', '4800',
+                        '19200', '38400', '57600','115200'])
+        self.layout.addRow(QLabel("COM Port:"),self.com_b_1)
+        self.layout.addRow(QLabel("Baudrate:"),self.com_b_2)
+        self.formGroupBox.setLayout(self.layout)
 
-def dialog():
-    mbox = QMessageBox()
-    mbox.setWindowTitle("Serial Communication")
-    mbox.setText("Serial Values")
-    serialPort = serial.Serial(port=serial_port_para['port'], 
-                           baudrate=int(serial_port_para['baudrate']),
-                           stopbits=serial.STOPBITS_ONE,
-                           bytesize=8, timeout=2)
-    
-    def count():
-        data = serialPort.readline()
-        data1 = data[:-2].decode("utf-8")
-        mbox.setDetailedText(f"{data1}")
-        QtCore.QTimer.singleShot(0, count)
-    count()       
-    mbox.exec_()
-    
-# ----------Window creation----------
-app = QApplication([])
-app.setStyle("Fusion")
+        self.com_b_1.activated.connect(self.set_serial_port)
+        self.com_b_2.activated.connect(self.set_baud_rate)
 
-w = QWidget()
-w.setWindowTitle("UART serial values")
-w.resize(300,300)
+        # creating display region
+        self.formGroupBox_1 = QGroupBox("Serial Values:")
+        self.layout_1 = QHBoxLayout()
+        self.lbl = QLabel()
+        self.layout_1.addWidget(self.lbl)
+        self.formGroupBox_1.setLayout(self.layout_1)
 
-vb = QVBoxLayout(w)
+        # Setting selection buttons
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
 
-lbl_com_b = QLabel(w)
-lbl_com_b.setText("Serial Port:")
-lbl_com_b.adjustSize()
-vb.addWidget(lbl_com_b)
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.formGroupBox)
+        self.mainLayout.addWidget(self.buttonBox)
+        self.mainLayout.addWidget(self.formGroupBox_1)
+        self.buttonBox.accepted.connect(self.add_form)
+        self.win.setLayout(self.mainLayout)
 
-com_b = QComboBox(w)
-com_b.addItems(serial_ports())
-vb.addWidget(com_b)
-com_b.activated.connect(set_serial_port)
+        self.win.show()
+        sys.exit(self.app.exec_())
 
-lbl_bd_rate = QLabel(w)
-lbl_bd_rate.setText("Baud Rate:")
-vb.addWidget(lbl_bd_rate)
+    def add_form(self):
+        self.serialPort = serial.Serial(port=serial_port_para['port'], 
+                            baudrate=int(serial_port_para['baudrate']),
+                            stopbits=serial.STOPBITS_ONE,
+                            bytesize=8, timeout=2)
+        def count():
+            data = self.serialPort.readline()
+            data1 = data[:-2].decode("utf-8")
+            self.lbl.setText(f"{data1}")
+            QtCore.QTimer.singleShot(2, count)
+            
+        count()
 
-com_b_1 = QComboBox(w)
-com_b_1.addItems(['9600', '1200', '2400', '4800',
-                 '19200', '38400', '57600','115200'])
-vb.addWidget(com_b_1)
-com_b_1.activated.connect(set_baud_rate)
+    def set_serial_port(self):
+        serial_port_para['port'] = self.com_b_1.currentText()
 
-btn = QPushButton(w)
-btn.setText('Enter')
-vb.addWidget(btn)
-btn.clicked.connect(dialog)
+    def set_baud_rate(self):
+        serial_port_para['baudrate'] = self.com_b_2.currentText()
 
-btn_stop = QPushButton(w)
-btn_stop.setText('Stop')
-vb.addWidget(btn_stop)
-btn_stop.clicked.connect(QApplication.quit)
+win = Window()
 
-w.show()
-app.exec_()
